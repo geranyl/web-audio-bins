bea0b460fbc29b0ae39a24a654f9297ab573ad93
Salpy <sekelian@gmail.com>
2015-01-17 20:42:55 -0500

    refactored into separate modules. ability to set your own ranges. scaling bass properly wip.

--- js/main.js
+++ js/main.js
@@ -5,254 +5,14 @@
 'use strict';
 
 
-var FFT_SIZE = 256; //256, 1024, 2048 (power of 2)
-
-var FT = {};
-
-
-FT.SoundCloud = function () {
-
-    if (!(this instanceof FT.SoundCloud))
-        return new FT.SoundCloud();
-
-
-    var self = this;
-    self.sound = {};
-    self.streamURL = '';
-    self.streamPlaylistIndex = 0;
-    self.initialized = false;
-
-    var init = function () {
-        SC.initialize({
-            client_id: CLIENT_ID
-        });
-        self.initialized = true;
-    };
-
-    self.getTrack = function (trackURL, successCb, failCb) {
-
-        if (!self.initialized) {
-            init();
-        }
-
-        SC.get('/resolve', {url: trackURL}, function (sound) {
-            if (sound.errors) {
-                var errorMessage = "";
-                for (var i = 0; i < sound.errors.length; i++) {
-                    errorMessage += sound.errors[i].error_message + '<br>';
-                }
-                errorMessage += 'Make sure the URL has the correct format: https://soundcloud.com/user/title-of-the-track';
-
-                console.log(errorMessage);
-                failCb(errorMessage);
-
-            } else {
-
-                if (sound.kind == "playlist") {
-                    self.sound = sound;
-                    self.streamURL = sound.tracks[self.streamPlaylistIndex].stream_url + '?client_id=' + CLIENT_ID;
-
-                    //success
-                    successCb(self.streamURL);
-                } else {
-                    self.sound = sound;
-                    self.streamURL = sound.stream_url + '?client_id=' + CLIENT_ID;
-
-                    //success
-                    successCb(self.streamURL);
-                }
-
-                console.log(self.streamURL)
-            }
-        });
-    }
-};
-
-
-FT.Player = function () {
-    if (!(this instanceof FT.Player))
-        return new FT.Player();
-
-    var self = this;
-    self.player = null;
-
-    self.init = function (playerDOMObject, onEndCb) {
-        self.player = playerDOMObject;
-        self.player.addEventListener('ended', function () {
-            onEndCb();
-        });
-
-    };
-
-    self.play = function (streamURL) {
-        self.player.setAttribute('src', streamURL);
-        self.player.play();
-    }
-};
-
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
-FT.FFT = function () {
-    if (!(this instanceof FT.FFT))
-        return new FT.FFT();
-
-
-    var self = this;
-
-    self.buckets = null;//low, mid, high
-
-    self.audioCtx = null;
-    self.analyzer = null;
-
-    self.init = function (playerDOM) {
-        self.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        // Webkit/blink browsers need prefix, Safari won't work without window.
-        self.analyzer = self.audioCtx.createAnalyser(); //create an analyzer
-        var source = self.audioCtx.createMediaElementSource(playerDOM);//get the source of audio
-        source.connect(self.analyzer); //connect the source to the analyzer
-        self.analyzer.connect(self.audioCtx.destination); //need to connect all nodes - e.g analyzer - to the context destination to hear audio
-
-        self.analyzer.fftSize = FFT_SIZE;
-        self.bufferLength = self.analyzer.frequencyBinCount; // half the FFT value
-        self.dataArray = new Uint8Array(self.bufferLength);
-    };
-
-
-    var curBucketIndex = 0;
-
-    var ranges = [];
-
-    self.setBuckets = function (numBuckets) {
-        if (numBuckets > self.analyzer.fftSize / 2) {
-            throw new Error('buckets cannot exceed half the fftSize');
-        }
-        //if(ranges.length < numBuckets*2){
-        //    throw new Error('you must specify a start and end value for each bucket');
-        //}
-
-        self.buckets = Array.apply(null, new Array(numBuckets)).map(Number.prototype.valueOf, 0);
-        //console.log(self.buckets)
-
-        for (var i = 0; i < numBuckets - 1; i++) {
-            ranges[i] = Math.ceil(self.bufferLength / numBuckets) * (i + 1);
-        }
-        ranges[numBuckets - 1] = self.bufferLength;
-
-        console.log(ranges, self.bufferLength)
-
-    };
-
-
-    self.measure = function () {
-
-        self.raf = window.requestAnimationFrame(self.measure);
-
-        self.analyzer.getByteFrequencyData(self.dataArray); //// get waveform data and put it into the dataArray; getByteTimeDomainData will give you the usual sine wave visual
-
-        var accumValue = 0;
-        var lastRangeValue = 0;
-        for (var i = 0; i < self.bufferLength; i++) {
-
-            //each sample point will be between 0 and 1;
-            accumValue += self.dataArray[i];
-
-            if (i + 1 >= ranges[curBucketIndex]) {
-                self.buckets[curBucketIndex] = accumValue / 256.0 / (ranges[curBucketIndex] - lastRangeValue);
-                lastRangeValue = ranges[curBucketIndex];
-                curBucketIndex++;
-                accumValue = 0;
-                if (curBucketIndex >= self.buckets.length) {
-                    curBucketIndex = 0;
-                }
-            }
-
-        }
-
-    };
-};
-
-
-FT.SampleVisual = function (analyzer, bufferLength, dataArray, buckets) {
-
-    if (!(this instanceof FT.SampleVisual))
-        return new FT.SampleVisual(analyzer, bufferLength, dataArray, buckets);
-
-
-    //temp
-    var canvasCtx = document.getElementById('canvas').getContext('2d');
-    var WIDTH = 400;
-    var HEIGHT = 400;
-
-    var self = this;
-
-    self.analyzer = analyzer;
-    self.dataArray = dataArray;
-    self.bufferLength = bufferLength;
-
-    this.draw = function () {
-        self.raf = requestAnimationFrame(self.draw);
-
-
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 1;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        var sliceWidth = WIDTH * 1.0 / buckets.length;
-        var x = 0;
-
-
-        for (var i = 0; i < buckets.length; i++) {
-            canvasCtx.fillStyle = 'hsl(' + 360 * (i / buckets.length) + ', 50%, 50%)';
-            canvasCtx.fillRect(x, HEIGHT - 10, sliceWidth, -buckets[i] * HEIGHT / 2 - 1);
-            x += sliceWidth;
-        }
-
-    };
-
-    //this.draw = function () {
-    //
-    //
-    //    requestAnimationFrame(self.draw);
-    //
-    //    self.analyzer.getByteTimeDomainData(self.dataArray);
-    //
-    //
-    //    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    //    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    //
-    //    canvasCtx.lineWidth = 2;
-    //    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-    //
-    //    canvasCtx.beginPath();
-    //
-    //    var sliceWidth = WIDTH * 1.0 / self.bufferLength;
-    //    var x = 0;
-    //
-    //    for (var i = 0; i < self.bufferLength; i++) {
-    //        var v = self.dataArray[i] / 128.0;
-    //        var y = v * HEIGHT / 2;
-    //
-    //        if (i === 0) {
-    //            canvasCtx.moveTo(x, y);
-    //        } else {
-    //            canvasCtx.lineTo(x, y);
-    //        }
-    //
-    //        x += sliceWidth;
-    //    }
-    //
-    //    canvasCtx.lineTo(WIDTH, HEIGHT / 2);
-    //    canvasCtx.stroke();
-    //};
-};
 
 
 window.onload = function () {
 
     /**where you do stuff**/
-    var useSoundCloud = false;
+    var useSoundCloud = true;
+
+
 
 
     var playerDOMElement = document.getElementById('player');
@@ -262,50 +22,81 @@ window.onload = function () {
         setTimeout(function(){
             window.cancelAnimationFrame(ft.raf);
             window.cancelAnimationFrame(visual.raf);
-        }, 1000);
+        }, 1);
 
     });
 
     var sc = FT.SoundCloud();
     var ft = FT.FFT();
+    var sampleRanges = [
+        ft.sampleRanges.initial,
+        ft.sampleRanges.bass,
+        ft.sampleRanges.bass,
+        ft.sampleRanges.low,
+        ft.sampleRanges.low,
+        ft.sampleRanges.mid,
+        ft.sampleRanges.mid,
+        ft.sampleRanges.umid,
+        ft.sampleRanges.umid,
+        ft.sampleRanges.hmid
+    ];
 
-
-    function playIt(streamURL) {
+    function playIt(streamURL, numBuckets, ranges) {
         player.play(streamURL);
         ft.init(playerDOMElement);
-        ft.setBuckets(128);
-
-
-        visual = FT.SampleVisual(ft.analyzer, ft.bufferLength, ft.dataArray, ft.buckets);
+        ft.setBuckets(numBuckets, ranges); //number of bars you'd want to see in the sample visual
+        visual = FT.SampleVisual(ft.buckets);
         visual.draw();
-
         ft.measure();
-
     }
 
 
     if (useSoundCloud) {
         //var track = 'https://soundcloud.com/infacted-recordings/shiv-r-pharmaceutical-grade';
-        //var track = 'https://soundcloud.com/nemo-iam/bobcaygeon-the-tragically-hip'
+        var track = 'https://soundcloud.com/nemo-iam/bobcaygeon-the-tragically-hip'
         //var track = 'https://soundcloud.com/thegrandarchitect/tools-sine-frequency-sweep';
-        var track = 'https://soundcloud.com/acle/4d-sounds-drum-test-quick-mix';
+        //var track = 'https://soundcloud.com/acle/4d-sounds-drum-test-quick-mix';
+        //var track = 'https://soundcloud.com/bassnectar/ellie-goulding-lights-bassnectar-remix';
+        //var track = 'https://soundcloud.com/sony-classical/sets/henrik-schwarz-instruments-1';
+        //var track = 'https://soundcloud.com/xavier-de-maistre/w-a-mozart-concerto-for-flute';
 
         sc.getTrack(track, function (streamURL) {
-            playIt(streamURL);
+            playIt(streamURL, null, sampleRanges);
         }, function (err) {
             console.log(err);
         });
     } else {
-        player.play('audiocheck.net_sweep20-20klin.mp3');
-        ft.init(playerDOMElement);
-        ft.setBuckets(8);
 
 
-        visual = FT.SampleVisual(ft.analyzer, ft.bufferLength, ft.dataArray, ft.buckets);
-        visual.draw();
+        playIt('audiocheck.net_sweep20-20klin.mp3', null, sampleRanges);
+        //playIt('audiocheck.net_sweep20-20klin.mp3', 5);
 
-        ft.measure();
     }
 
-
 };
+
+
+/**
+ * http://www.decibelcar.com/menutheory/141-treble-hertz.html
+ * Bass
+ (Approximately 20hz-140hz)
+ The 60hz-90hz range is where we notice the greatest perceptible changes in "bass response." Try a test tone and see just how well you hear 20hz or even 32hz, compared with the same volume of 60hz or 90hz.
+
+ Mid-Bass
+ (Approximately 140hz-400hz)
+ Mid bass has lots of instruments included in its frequency range. Cello, Bassoon, and Male Voice are samples of "mid-bass" sounds. This is also where most 'bass' controls can make a mess of your music. Too much mid-bass gives a muddy and "boomy" quality. Too little and it sounds hollow and thin.
+
+ Midrange
+ (Approximately 400hz-2.6khz)
+ Since our ears are most sensitive to midrange frequencies, midrange has the greatest effect on the overall sound of your stereo system. The "proper" settings are the ones most pleasing to you the listener. Female vocals and Bass Tuba are examples of mirange sounds.
+
+
+ Upper Midrange
+ (Approximately 2.6khz-5.2khz)
+ Pipe organ and piano are examples of this sound range. Speaker designers often boost output in this range to increase "presence" of the music. Too much gives a sound that is overbearingly harsh and fatigues the ears. A balance between this frequency range and the midrange frequencies gives the most pleasing sound.
+
+ High End
+ (Approximately 5.2khz-20khz - Two Regions)
+ 5.2khz up to about 12khz is the realm of dreaded treble control. This is where harmonics can really enhance your musical enjoyment. It's this range that affects the brilliance of music. Too much gives an unpleasant, harsh and "piercing" (painful) quality to your music.
+
+ **/
